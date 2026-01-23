921769ecdb1885b0086d42d4123b5bd8dc815c1a
Jan Schopohl <git@janschopohl.de>
2026-01-23 21:43:03 +0100

    Implement ICanvas in new CadCanvas

--- CADability.Avalonia/CadCanvas.axaml.cs
+++ CADability.Avalonia/CadCanvas.axaml.cs
@@ -1,14 +1,28 @@
-using System;
-using System.Numerics;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.OpenGL;
 using Avalonia.OpenGL.Controls;
+using CADability.GeoObject;
+using CADability.Substitutes;
+using CADability.UserInterface;
+using System;
+using System.Numerics;
 using static Avalonia.OpenGL.GlConsts;
 
 namespace CADability.Avalonia
 {
-    public partial class CadCanvas: UserControl
+    public partial class CadCanvas: UserControl, ICanvas
     {
+        private static CADability.Substitutes.Rectangle Subst(Rect v)
+        {
+            return new Substitutes.Rectangle((int)v.X, (int)v.Y, (int)v.Width, (int)v.Height);
+        }
+
+        private IPaintTo3D paintTo3D;
+        private IFrame frame;
+        private IView view;
+        private String currentCursor;
+
         public CadCanvas()
         {
             InitializeComponent();
@@ -16,5 +30,48 @@ namespace CADability.Avalonia
             PaintToOpenGL openGlControl = new PaintToOpenGL();
             this.Content = openGlControl;
         }
+
+        void ICanvas.Invalidate() {}
+
+        Rectangle ICanvas.ClientRectangle => Subst(base.Bounds);
+
+        IFrame ICanvas.Frame
+        {
+            get { return frame; }
+        }
+
+        string ICanvas.Cursor
+        {
+            get { return currentCursor; }
+            set { currentCursor = value; } //TODO
+        }
+
+        IPaintTo3D ICanvas.PaintTo3D
+        {
+            get { return paintTo3D; }
+        }
+
+        public event Action<ICanvas> OnPaintDone;
+
+        void ICanvas.ShowView(IView toShow) {}
+
+        IView ICanvas.GetView()
+        {
+            return view;
+        }
+
+        Substitutes.Point ICanvas.PointToClient(Substitutes.Point mousePosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICanvas.ShowContextMenu(MenuWithHandler[] contextMenu, Substitutes.Point viewPosition, System.Action<int> collapsed) {}
+
+        Substitutes.DragDropEffects ICanvas.DoDragDrop(GeoObjectList dragList, Substitutes.DragDropEffects all)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICanvas.ShowToolTip(string toDisplay) {}
     }
 }
